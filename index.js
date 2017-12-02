42f970bb059ee9728a932224823105f53e6b9776
Tortuvshin Byambaa <toroo.byamba@gmail.com>
2017-12-02 15:57:15 +0000

    intelligo integration

--- index.js
+++ index.js
@@ -4,64 +4,15 @@ const CryptoBot = require('intelligo'),
       config = require( 'config'),
       request = require( 'request');
 
-function receivedMessage(event) {
-  var senderID = event.sender.id;
-  var recipientID = event.recipient.id;
-  var timeOfMessage = event.timestamp;
-  var message = event.message;
 
-  console.log("Received message for user %d and page %d at %d with message:", 
-    senderID, recipientID, timeOfMessage);
-  console.log(JSON.stringify(message));
+const bot = new CryptoBot({
+  accessToken: config.get('pageAccessToken'),
+  verifyToken: config.get('validationToken'),
+  appSecret: config.get('appSecret')
+});
 
-  var isEcho = message.is_echo;
-  var messageId = message.mid;
-  var appId = message.app_id;
-  var metadata = message.metadata;
-
-  var messageText = message.text;
-  var messageAttachments = message.attachments;
-  var quickReply = message.quick_reply;
-
-  if (isEcho) {
-    console.log("Received echo for message %s and app %d with metadata %s", 
-      messageId, appId, metadata);
-    return;
-  } else if (quickReply) {
-    var quickReplyPayload = quickReply.payload;
-    console.log("Quick reply for message %s with payload %s",
-      messageId, quickReplyPayload);
-
-    sendTextMessage(senderID, "Quick reply tapped");
-    return;
-  }
-
-  if (messageText) {
-    
-    if (textMatches(messageText, "get started")) 
-        sendWelcome(senderID);
-        
-    else if (textMatches(messageText, "btc")) 
-      sendPrice(senderID, messageText);
-    else if (textMatches(messageText, "eth")) 
-      sendPrice(senderID, messageText);
-    else if (textMatches(messageText, "ltc")) 
-      sendPrice(senderID, messageText);
-    else if (textMatches(messageText, "read receipt")) 
-      sendReadReceipt(senderID);
-    else if (textMatches(messageText, "typing on")) 
-      sendTypingOn(senderID);
-    else if (textMatches(messageText, "typing off")) 
-      sendTypingOff(senderID);
-    else if (textMatches(messageText, "help")) 
-      sendHelp(senderID);
-    else
-      sendWelcome(senderID);
-    
-  } else if (messageAttachments) {
-    sendTextMessage(senderID, "Message with attachment received");
-  }
-}
+bot.start(8000);
+bot.learnAI();
 
 function sendPrice(receiptId, messageData) {
   request({
